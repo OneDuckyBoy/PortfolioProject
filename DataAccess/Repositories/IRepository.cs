457f021dfac4f204cbb2ec6045a8732526a7b4c1
OneDuckyBoy <stilianmatev@gmail.com>
2025-01-24 14:05:09 +0200

    Add project files.

---
+++ DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolio.Model.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        T Add(T entity);
+        T Update(T entity);
+        void Delete(int id);
+        bool EntityExists(int id);
+
+    }
+}
---
+++ DataAccess/Repositories/Repository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Portfolio.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace portfolio.Model.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public T Add(T entity)
+        {
+            T t = _context.Set<T>().Add(entity).Entity;
+            _context.SaveChanges();
+            return t;
+        }
+
+        public void Delete(int id)
+        {
+            T entity = GetById(id);
+            _context.Set<T>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _context.Set<T>();
+        }
+
+        public T GetById(int id)
+        {
+            return _context.Set<T>().Find(id);
+
+        }
+
+        public T Update(T entity)
+        {
+            T entity1 = _context.Set<T>().Update(entity).Entity;
+            _context.SaveChanges();
+            return entity1;
+        }
+        public bool EntityExists(int id)
+        {
+            return _context.Set<T>().Find(id) != null;
+        }
+    }
+}
