2dacda3d7b78440312892b770a140041fe1c874c
OneDuckyBoy <stilianmatev@gmail.com>
2025-01-27 23:26:48 +0200

    added services

--- portfolio/Program.cs
+++ portfolio/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
-using portfolio.Model.Repositories;
+using Core.Repositories;
+using Core;
 using Portfolio.Data;
 
 var builder = WebApplication.CreateBuilder(args);
