aca8890fc6331243f4c1e4c3b6462f2c3bfea9b1
OneDuckyBoy <stilianmatev@gmail.com>
2025-01-28 01:56:58 +0200

    added the rest of the crud operations for the Categories

---
+++ DataAccess/Migrations/20250127230001_removedSomeTables.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class removedSomeTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_MoreResources_MoreResourceId",
+                table: "Images");
+
+            migrationBuilder.DropTable(
+                name: "ResourceLinks");
+
+            migrationBuilder.DropTable(
+                name: "MoreResources");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_MoreResourceId",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "MoreResourceId",
+                table: "Images");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MoreResourceId",
+                table: "Images",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "MoreResources",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MoreResources", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ResourceLinks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MoreResourceId = table.Column<int>(type: "int", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResourceLinks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ResourceLinks_MoreResources_MoreResourceId",
+                        column: x => x.MoreResourceId,
+                        principalTable: "MoreResources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_MoreResourceId",
+                table: "Images",
+                column: "MoreResourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceLinks_MoreResourceId",
+                table: "ResourceLinks",
+                column: "MoreResourceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_MoreResources_MoreResourceId",
+                table: "Images",
+                column: "MoreResourceId",
+                principalTable: "MoreResources",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
--- portfolio/Controllers/CategoryController.cs
+++ portfolio/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Portfolio.Models;
 
 namespace Portfolio.Controllers
@@ -22,6 +23,65 @@ namespace Portfolio.Controllers
         {
             var category = _categoryService.GetById(id);
             return View(category);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _categoryService.Add(category);
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category =  _categoryService.GetById(id);
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Category category)
+        {
+            _categoryService.Delete(category.Id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var category = _categoryService.GetById(id);
+            return View(category);
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit( int id,Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                    _categoryService.Update(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+
+
+        }
+
+        private bool CategoryExists(int id)
+        {
+            return _categoryService.EntityExists(id);
         }
     }
 }
