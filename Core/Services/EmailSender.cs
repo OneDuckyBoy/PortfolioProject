7e27a7862d94cbddb5a73c596cf7ef9bf8e50af5
OneDuckyBoy <stilianmatev@gmail.com>
2025-01-31 01:32:46 +0200

    added dummy email service

---
+++ Core/Services/EmailSender.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace Core.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // Implement your email sending logic here
+            return Task.CompletedTask;
+        }
+    }
+}
